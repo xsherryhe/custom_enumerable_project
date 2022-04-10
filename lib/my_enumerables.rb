d0e5af053973d76a3937e433d5955091af217d5c
Sherry He <xsherryhe@gmail.com>
2022-04-10 14:02:05 -0400

    Completed half of custom enumerable methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,32 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    for i in 0...self.size
+      yield(self[i], i)
+    end
+    self
+  end
+
+  def my_select
+    new_arr = []
+    my_each { |elem| new_arr << elem if yield(elem) }
+    new_arr
+  end
+
+  def my_all?
+    my_each { |elem| return false unless yield(elem) }
+    true
+  end
+
+  def my_any?
+    my_each { |elem| return true if yield(elem) }
+    false
+  end
+
+  def my_none?
+    my_each { |elem| return false if yield(elem) }
+    true
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +35,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for i in 0...self.size
+      yield(self[i])
+    end
+    self
+  end
 end
